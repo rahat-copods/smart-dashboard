d29169c81cb771a51670ce43283f917267b80170
Rahat Sayyed <219821091+rahat-sayyed@users.noreply.github.com>
2025-08-12 12:07:54 +0530

    fix: exported all env from next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    AI_MODEL_NAME: process.env.AI_MODEL_NAME,
+    AI_BASE_URL: process.env.AI_BASE_URL,
+    AI_API_KEY: process.env.AI_API_KEY,
+    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
     AUTH_TOKEN: process.env.AUTH_TOKEN,
+    POSTGRES_URL: process.env.POSTGRES_URL,
   },
 };
 
