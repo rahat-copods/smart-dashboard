3947c7307e27fb92474e3624d16a01ecdd3c31c9
Rahat Sayyed <219821091+rahat-copods@users.noreply.github.com>
2025-08-13 09:56:07 +0530

    cors added

--- next.config.js
+++ next.config.js
@@ -9,6 +9,30 @@ const nextConfig = {
     AUTH_TOKEN: process.env.AUTH_TOKEN,
     POSTGRES_URL: process.env.POSTGRES_URL,
   },
+  async headers() {
+    return [
+        {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "https://dev.d27evysfjs548k.amplifyapp.com" },
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        },
+        {
+            // For your main branch
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "https://main.d27evysfjs548k.amplifyapp.com" },
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+}
 };
 
 module.exports = nextConfig;
